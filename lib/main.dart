2b7eff0e632a8e1d66ef1cac83aa527406f4a13b
habibeliaedu-blip <habibelia.edu@gmail.com>
2025-11-26 11:21:42 +0000

    Rename app to JOKKO DIMBALI and configure icons

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Jokko Dimbali',
+      title: 'JOKKO DIMBALI',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF4A4F58)),
